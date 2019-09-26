1c626e9fa6c3a07894f920c4376c0573e638e457
Unni Ramachandran <unnipbvr@gmail.com>
2019-09-26 19:23:08 -0400

    Removed id columns from models since it was not having auto increment.
    Added a seeds.sql for UserRole, User and Product
    
    Setup api routes for ProductCatalog table

--- models/OrderHeader.js
+++ models/OrderHeader.js
@@ -2,11 +2,6 @@
 
 module.exports = function(sequelize, DataTypes) {
 	return sequelize.define('OrderHeader', {
-		id: {
-			type: DataTypes.INTEGER(11),
-			allowNull: false,
-			primaryKey: true
-		},
 		order_user_id: {
 			type: DataTypes.INTEGER(11),
 			allowNull: false,
--- models/user.js
+++ models/user.js
@@ -2,11 +2,6 @@
 
 module.exports = function(sequelize, DataTypes) {
 	return sequelize.define('User', {
-		id: {
-			type: DataTypes.INTEGER(11),
-			allowNull: false,
-			primaryKey: true
-		},
 		user_name: {
 			type: DataTypes.STRING(20),
 			allowNull: false
