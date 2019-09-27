105d01a2e96c2fbdc703bdf2f476ec417349c267
Judy <judys717@hotmail.com>
2019-09-26 23:30:23 -0400

    Added code for order routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,87 @@
 var db = require("../models");
 
-module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
-    });
+module.exports = function (app) {
+
+  // Get all orders
+  app.get("/api/orders", function (req, res) {
+    console.log("Get All Orders");
+    db.OrderHeader.findAll({ include: [User] })
+      .then(function (orderList) {
+        res.json(orderList);
+      }).catch(function (error) {
+        res.sendStatus(500);
+      });
   });
 
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
-    });
+  // Get an order with it's order details and user info
+  app.get("/api/orders/:id", function (req, res) {
+    console.log("Get an order and order details");
+    db.OrderHeader.findAll({ where: { id: req.params.id }, include: [OrderDetail, User] })
+      .then(function (orderList) {
+        res.json(orderList);
+      }).catch(function (error) {
+        res.sendStatus(500);
+      });
   });
 
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
-    });
+  // Get user's order
+  app.get("/api/orders/user/:id", function (req, res) {
+    console.log("Get an order and order details");
+    db.OrderHeader
+      .findAll({ include: [{ model: User, where: { id: req.params.id } }] })
+      .then(function (orderList) {
+        res.json(orderList);
+      }).catch(function (error) {
+        res.sendStatus(500);
+      });
+  });
+
+  /* Create a new Order 
+    {
+        "order_user_id": 1,
+        "order_supplier_id": 20,
+        "order_item_count": 3,
+        "order_status": "Open",
+        "OrderDetail": [{
+          "product_id": 25,
+          "quantity": 1         
+        },
+        {
+          "product_id": 56,
+          "quantity": 10         
+        }]
+      }
+
+  */
+  app.post("/api/orders", function (req, res) {
+    console.log("Create an order");
+    db.OrderHeader.create(req.body)
+      .then(function (createdOrder) {
+        res.sendStatus(200);
+      }).catch(function (error) {
+        res.sendStatus(400);
+      });;
+  });
+
+  // Delete an order using id
+  app.delete("/api/orders/:id", function (req, res) {
+    console.log("Delete an order");
+    db.OrderHeader.destroy({ where: { id: req.params.id } })
+      .then(function (deletedOrder) {
+        res.sendStatus(200);
+      }).catch(function (error) {
+        res.sendStatus(500);
+      });;
+  });
+
+  // Updates an order status
+  app.put("/api/orders", function (req, res) {
+    console.log("Updates an order status");
+    db.OrderHeader.update({ order_status: req.body.order_status }, { where: { id: req.params.id } })
+      .then(function (deletedOrder) {
+        res.sendStatus(200);
+      }).catch(function (error) {
+        res.sendStatus(500);
+      });;
   });
 };
