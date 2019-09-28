5ebf727b270d01f293a2921d4497353d9709a1d1
gitMarcldo <marcldo@users.noreply.github.com>
2019-09-28 14:38:43 -0400

    added img to prodCat table, costco and 4 products

--- models/productCatalog.js
+++ models/productCatalog.js
@@ -1,54 +1,58 @@
 /* jshint indent: 1 */
 
 module.exports = function (sequelize, DataTypes) {
-	var ProductCatalog = sequelize.define('ProductCatalog', {
-		product_name: {
-			type: DataTypes.STRING(45),
-			allowNull: false
-		},
-		product_description: {
-			type: DataTypes.STRING(200),
-			allowNull: true
-		},
-		product_expiry_date: {
-			type: DataTypes.DATEONLY,
-			allowNull: false,
-			defaultValue: '9999-12-31'
-		},
-		product_perishable: {
-			type: DataTypes.INTEGER(4),
-			allowNull: false,
-			defaultValue: 0
-		},
-		product_original_qty: {
-			type: DataTypes.INTEGER(11),
-			allowNull: false
-		},
-		product_current_qty: {
-			type: DataTypes.INTEGER(11),
-			allowNull: false,
-			defaultValue: '0'
-		},
-		supplier_id: {
-			type: DataTypes.INTEGER(11),
-			allowNull: false,
-			references: {
-				model: 'User',
-				key: 'id'
-			}
-		},
-		product_posted_date: {
-			type: DataTypes.DATEONLY,
-			allowNull: false
-		}
-	}, {
-		tableName: 'ProductCatalog'
-	});
+  var ProductCatalog = sequelize.define("ProductCatalog", {
+    product_name: {
+      type: DataTypes.STRING(45),
+      allowNull: false
+    },
+    product_description: {
+      type: DataTypes.STRING(200),
+      allowNull: true
+    },
+    product_img: {
+      type: DataTypes.STRING(500),
+      allowNull: true
+    },
+    product_expiry_date: {
+      type: DataTypes.DATEONLY,
+      allowNull: false,
+      defaultValue: "9999-12-31"
+    },
+    product_perishable: {
+      type: DataTypes.INTEGER(4),
+      allowNull: false,
+      defaultValue: 0
+    },
+    product_original_qty: {
+      type: DataTypes.INTEGER(11),
+      allowNull: false
+    },
+    product_current_qty: {
+      type: DataTypes.INTEGER(11),
+      allowNull: false,
+      defaultValue: "0"
+    },
+    supplier_id: {
+      type: DataTypes.INTEGER(11),
+      allowNull: false,
+      references: {
+        model: "User",
+        key: "id"
+      }
+    },
+    product_posted_date: {
+      type: DataTypes.DATEONLY,
+      allowNull: false
+    }
+  }, {
+      tableName: "ProductCatalog"
+    });
 
-	ProductCatalog.associate = function (models) {
-		models.ProductCatalog.belongsTo(models.User, { as: 'Vendor', foreignKey: { name: 'supplier_id', allowNull: false } });
-		models.ProductCatalog.hasMany(models.OrderDetail, { foreignKey: { name: 'product_id', allowNull: false } });
-	}
+  ProductCatalog.associate = function (models) {
+    models.ProductCatalog.belongsTo(models.User, { as: "Vendor", foreignKey: { name: "supplier_id", allowNull: false } });
+    models.ProductCatalog.hasMany(models.OrderDetail, { foreignKey: { name: "product_id", allowNull: false } });
+  };
 
-	return ProductCatalog;
+  return ProductCatalog;
 };
--- models/seeds.sql
+++ models/seeds.sql
@@ -52,11 +52,22 @@ INSERT INTO `foodbank_db`.`User`
 VALUES
 ("Jane", "abcd",2,now(), now());
 
+INSERT INTO `foodbank_db`.`User`
+(
+`user_name`,
+`user_password`,
+`user_role_id`,
+`createdAt`,
+`updatedAt`)
+VALUES
+("Alex", "abcd",2,now(), now());
+
 
 INSERT INTO `foodbank_db`.`ProductCatalog`
 (
 `product_name`,
 `product_description`,
+`product_img`,
 `product_expiry_date`,
 `product_perishable`,
 `product_original_qty`,
@@ -69,6 +80,7 @@ VALUES
 (
 "Bananas",
 "Yellow Bananas",
+"https://5.imimg.com/data5/CI/VG/MY-59453495/yellow-banana-500x500.jpg",
 "2019-12-31",
 1,
 100,
@@ -78,6 +90,91 @@ VALUES
 now(),
 now()
 );
+
+INSERT INTO `foodbank_db`.`ProductCatalog`
+(
+`product_name`,
+`product_description`,
+`product_img`,
+`product_expiry_date`,
+`product_perishable`,
+`product_original_qty`,
+`product_current_qty`,
+`supplier_id`,
+`product_posted_date`,
+`createdAt`,
+`updatedAt`)
+VALUES
+(
+"Apples",
+"Red Apples",
+"https://www.biggro.com/content/images/thumbs/0009245_apple_550.png",
+"2019-12-31",
+1,
+100,
+100,
+1,
+"2019-09-26",
+now(),
+now()
+);
+
+INSERT INTO `foodbank_db`.`ProductCatalog`
+(
+`product_name`,
+`product_description`,
+`product_img`,
+`product_expiry_date`,
+`product_perishable`,
+`product_original_qty`,
+`product_current_qty`,
+`supplier_id`,
+`product_posted_date`,
+`createdAt`,
+`updatedAt`)
+VALUES
+(
+"Canned Tomatoes",
+"Diced Canned Tomatoes",
+"https://i5.walmartimages.ca/images/Enlarge/118/040/118040.jpg",
+"2019-12-31",
+1,
+100,
+100,
+2,
+"2019-09-26",
+now(),
+now()
+);
+
+INSERT INTO `foodbank_db`.`ProductCatalog`
+(
+`product_name`,
+`product_description`,
+`product_img`,
+`product_expiry_date`,
+`product_perishable`,
+`product_original_qty`,
+`product_current_qty`,
+`supplier_id`,
+`product_posted_date`,
+`createdAt`,
+`updatedAt`)
+VALUES
+(
+"Canned Tuna",
+"Smoked Albacore Tuna",
+"https://www.stjeans.com/wp-content/uploads/2014/05/can-smoked-tuna-100g2.jpg",
+"2019-12-31",
+1,
+100,
+100,
+2,
+"2019-09-26",
+now(),
+now()
+);
+
 
 
 INSERT INTO `foodbank_db`.`OrderHeader` 
