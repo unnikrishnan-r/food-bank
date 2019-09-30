8ca368300de7dd4e75c771626a46e7797787185b
Judy <judys717@hotmail.com>
2019-09-30 00:21:17 -0400

    Added js file

---
+++ public/js/customerDashboard.js
@@ -0,0 +1,19 @@
+/* eslint-disable camelcase */
+
+$(document).ready(function () {
+
+    $(".place-order").on("click", function () {
+        var newCart = {
+            cart_owner_id: 1,
+            cart_status: "Open",
+            product_id: $(this).data("product-id"),
+            quantity: 1
+        };
+
+        $.post("/api/cart/user", newCart)
+            .then(results => {
+                console.log(results);
+            });
+    });
+
+});
