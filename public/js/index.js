092ff8f33e69442f7502af26fe1d986736845660
gitMarcldo <marcldo@users.noreply.github.com>
2019-09-29 23:33:36 -0400

    finished homepage

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,19 @@
 $(document).ready(function () {
   $(".header").height($(window).height());
+
+  $(".navbar a").click(function () {
+    $("body,html").animate({
+      scrollTop: $("#" + $(this).data("value")).offset().top
+    }, 1000);
+
+  });
+
+  $(".header a").click(function () {
+    $("body,html").animate({
+      scrollTop: $("#" + $(this).data("value")).offset().top
+    }, 1000);
+
+  });
 });
 
 
