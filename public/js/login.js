26d7a61588b1838ed30121833810cfef6b7adc32
Unni Ramachandran <unnipbvr@gmail.com>
2019-10-01 14:14:40 -0400

    Login screen changes. Still not 100%, the customer dashboard API call returns html code instead of rendering

--- public/js/login.js
+++ public/js/login.js
@@ -1,24 +1,43 @@
-$(document).ready(function () {
-  $("#login-btn").on("click", function (event) {
+$(document).ready(function() {
+  $("#login-btn").on("click", function(event) {
     event.preventDefault();
-
     var user = {
       // eslint-disable-next-line camelcase
-      user_name: $("#username").val().trim(),
+      user_name: $("#username")
+        .val()
+        .trim(),
       // eslint-disable-next-line camelcase
-      user_password: $("#password").val().trim()
+      user_password: $("#password")
+        .val()
+        .trim()
     };
 
     // Send the PUT request.
-    $.ajax("", {
-      type: "get",
+    $.ajax("/api/login", {
+      type: "post",
       data: user
-    })
-      .then(
-        function () {
-          console.log("logged in");
+    }).then(function(user) {
+      if (user) {
+        console.log("logged in as ", user.userRole, user.userId);
+        switch (user.userRole) {
+        case "Vendor":
+            $.ajax(`/api/products/vendor/${user.userId}`, {
+              type: "GET"
+          }).then(data => console.log(data));
+          break;
+        case "Buyer":
+          $.ajax(`/api/products/buyer/${user.userId}`, {
+            type: "GET"
+          });
+          break;
 
+        default:
+          console.log("Default");
+          break;
         }
-      );
+      } else {
+        console.log("Log in failed");
+      }
+    });
   });
 });
