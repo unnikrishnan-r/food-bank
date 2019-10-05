d4d3184d00f69cf78e0bb9d3f8f83c9de3d837c3
Judy <judys717@hotmail.com>
2019-10-05 08:29:06 -0400

    Added username and logout

--- public/js/buyer.js
+++ public/js/buyer.js
@@ -1,9 +1,17 @@
 $(document).ready(function () {
 
-    $("#nav-orders").on("click", function (event) {
-        event.preventDefault();
+  $("#nav-orders").on("click", function (event) {
+    event.preventDefault();
 
-        location.href = "/api/orders/buyer/" + localStorage.getItem("userId");
-    });
+    location.href = "/api/orders/buyer/" + localStorage.getItem("userId");
+  });
 
+  $("#nav-user").text("Welcome " + localStorage.getItem("userName"));
+
+  $("#nav_logout").on("click", function (event) {
+    event.preventDefault();
+
+    localStorage.clear();
+    location.href = "/";
+  });
 });
--- public/js/vendor.js
+++ public/js/vendor.js
@@ -12,4 +12,13 @@ $(document).ready(function () {
     location.href = "/api/orders/vendor/" + localStorage.getItem("userId");
   });
 
+  $("#nav-user").text("Welcome " + localStorage.getItem("userName"));
+
+  $("#nav_logout").on("click", function (event) {
+    event.preventDefault();
+
+    localStorage.clear();
+    location.href = "/";
+  });
+
 });
