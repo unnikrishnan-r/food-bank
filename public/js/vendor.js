fcd3dbeb4107c35d3b056eaf94640245ce07579f
Unni Ramachandran <unnipbvr@gmail.com>
2019-10-06 11:28:48 -0400

    Added a modal and logic for deleting products if its not tied to any open orders

--- public/js/vendor.js
+++ public/js/vendor.js
@@ -21,13 +21,34 @@ $(document).ready(function () {
     location.href = "/";
   });
 
-  $(".delete-product").on("click", function(event){
+  $(".delete-product").on("click", function(event) {
     event.preventDefault();
     console.log(`Trying to delete product ${$(this).attr("product-id")}`);
-    $.ajax(`/api/countProductsInOrder/${$(this).attr("product-id")}`)
-    .then(function (response) {
-      console.log(`Product is present in ${response.openOrderCount} unfulfilled orders`)
+    var productIdToBeDeleted = $(this).attr("product-id");
+    $.ajax(`/api/countProductsInOrder/${productIdToBeDeleted}`).then(function(
+      response
+    ) {
+      console.log(
+        `Product is present in ${response.openOrderCount} unfulfilled orders`
+      );
+      if (response.openOrderCount > 0) {
+        $("#modal-title-message").text("Unable to Delete");
+        $("#productDeleteMessage").text(
+          "Cannot delete the product, there are open orders for it. Sorry!"
+        );
+        $("#unableToDelete").modal();
+        console.log("ffff");
+      } else {
+        $.ajax(`/api/products/${productIdToBeDeleted}`, {
+          type: "DELETE"
+        })
+          .then(results => {
+            location.reload();
+          })
+          .fail(function() {
+            console.log("Error");
+          });
+      }
     });
-
   });
 });
